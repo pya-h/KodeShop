c681a72e27e5c8a4bc04f9f2ac41402c076471e1
pya-h <p4ya.py@gmail.com>
2024-12-09 14:58:24 +0330

    designed video page too

--- post/models.py
+++ post/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django_ckeditor_5.fields import CKEditor5Field
 from user.models import User
 from django.urls import reverse
+from enum import Enum
 
 
 class PostBase(models.Model):
@@ -20,7 +21,7 @@ class PostBase(models.Model):
 
 
 class BlogPost(PostBase):
-    thumbnail = models.ImageField(upload_to='posts/blogs', blank=True, null=True,  verbose_name='تصویر')
+    thumbnail = models.ImageField(upload_to='posts/blogs', blank=True, null=True, verbose_name='تصویر')
     body = CKEditor5Field('متن پست', config_name='default')
 
     class Meta:
@@ -28,20 +29,37 @@ class BlogPost(PostBase):
         verbose_name_plural = 'پست بلاگ'
 
     def url(self):
-        return reverse('blog_post', args = [self.id])
+        return reverse('blog_post', args=[self.id])
+
+
+class VideoQualityEnum(Enum):
+    LOW = 0
+    MEDIUM = 1
+    HIGH = 2
 
 
 class VideoPost(PostBase):
-    thumbnail = models.ImageField(upload_to='posts/videos/thumbnails', blank=True, null=True, verbose_name='تصویر پیش نمایش')
+    thumbnail = models.ImageField(upload_to='posts/videos/thumbnails', blank=True, null=True,
+                                  verbose_name='تصویر پیش نمایش')
     caption = models.TextField(blank=True, null=True, verbose_name='کپشن ویدیو')
     original_video = models.FileField(upload_to='videos/', verbose_name='فایل ویدیو (کیفیت اصلی) *')
     low_quality_video = models.FileField(upload_to='videos/low/', blank=True, null=True,
                                          verbose_name='نسخه کیفیت پایین ویدیو (اختیاری)')
     medium_quality_video = models.FileField(upload_to='videos/medium/', blank=True, null=True,
                                             verbose_name='نسخه کیفیت متوسط ویدیو (اختیاری)')
+
     class Meta:
         verbose_name = 'ویدیو بلاگ'
         verbose_name_plural = 'ویدیو بلاگ'
 
     def url(self):
-        return reverse('blog_video', args = [self.id])
+        return reverse('video_post', args=[self.id])
+
+    def get(self, quality: int = 2):
+        match quality:
+            case VideoQualityEnum.LOW.value:
+                return self.low_quality_video.url
+            case VideoQualityEnum.MEDIUM.value:
+                return self.medium_quality_video.url
+            case _:
+                return self.original_video.url
--- post/urls.py
+++ post/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('blogs/<int:blog_id>/', views.show_blog, name="blog_post"),
     path('blogs/', views.list_blog_posts, name="blogs"),
-    path('videos/<int:video_post_id>/', views.list_video_posts, name="video_post"),
+    path('videos/<int:video_post_id>/', views.show_video, name="video_post"),
     path('videos/', views.list_video_posts, name="videos"),
 ]
